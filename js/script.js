a68fe308582bb09a81a8849b29406c9adc6127c7
Alexej <39061639+alexejklass@users.noreply.github.com>
2021-03-01 11:36:49 +0100

    changes

--- js/script.js
+++ js/script.js
@@ -11,196 +11,70 @@ let play = document.querySelector('.play')
 let space = document.querySelector('.space')
 let mars = document.querySelector('.mars')
 let earth = document.querySelector('.earth')
-let lives = document.querySelector('.lives')
+let lifes = document.querySelector('.lifes')
+let videoContainer = document.querySelector('.videoContainer')
+let videoSource = videoContainer.querySelector('source')
+console.log(videoSource)
 
 let death = 3
 let stars = ['⭐', '⭐', '⭐']
+
+//Music playback start after 3 seconds
+let musicPlay = setTimeout(() => {
+  audio.play()
+  audio.volume = 0.01
+}, 3000)
+
+//Display stars
 let showStars = () => {
-  lives.textContent = ''
-  stars.forEach(el => {
-    lives.textContent += el
+  lifes.textContent = ''
+  stars.forEach(star => {
+    lifes.textContent += star
   })
 }
-showStars()
 
+//Stop music
 stop.addEventListener('click', () => {
   audio.pause()
   audio.currentTime = 0
 })
-window.addEventListener('DOMContentLoaded', () => {
-  setTimeout(() => {
-    audio.volume = 0.01
-    audio.play()
-  }, 3000)
-})
 
-let asteroidFall = (randomNumber, asteroid, laserFunction) => {
-  timer = setInterval(() => {
-    let positionAsteroid = asteroid.offsetTop
-    asteroid.style.top = positionAsteroid + randomNumber + 'px'
-    console.log(positionAsteroid)
-    if (positionAsteroid >= window.innerHeight) {
-      if (death === 0) {
-        gameover.style.display = 'flex'
-        container.removeChild(asteroid)
-        clearInterval(timer)
-      } else if (death > 0) {
-        death = death - 1
-
-        container.removeChild(asteroid)
-        window.clearInterval(timer)
-        window.clearInterval(laserFunction.interval)
-        stars.pop()
-        showStars()
-        createAsteroid()
-      }
-    }
-  }, 20)
-}
-
-let laserFunction = asteroidFall => {
-  lasersound.pause()
-  lasersound.currentTime = 0
-  lasersound.volume = 0.01
-  lasersound.play()
-  asteroid = document.querySelector('.asteroid')
-  let positionShipX = ship.offsetLeft
-  let positionShipY = ship.offsetTop
-  let laser = document.createElement('div')
-  laser.classList.add('laser')
-  laser.innerHTML = '🔥'
-  container.insertAdjacentElement('beforeend', laser)
-  laser.style.left = positionShipX + 'px'
-  laser.style.top = positionShipY - 90 + 'px'
-  let positionLaserY = laser.offsetTop
-  let positionLaserX = laser.offsetLeft
-  let positionAsteroidY = asteroid.offsetTop
-  let positionAsteroidX = asteroid.offsetLeft
-  interval = setInterval(() => {
-    positionLaserY = laser.offsetTop
-    positionLaserX = laser.offsetLeft
-    positionAsteroidY = asteroid.offsetTop + 10
-    positionAsteroidX = asteroid.offsetLeft
-    let asteroidSizeChange = asteroid.style.fontSize
-    let asteroidRem = asteroidSizeChange.split('rem')
-    if (positionLaserY < positionAsteroidY) {
-      if (
-        positionLaserX > positionAsteroidX - asteroid.offsetWidth / 2 &&
-        positionLaserX < positionAsteroidX + asteroid.offsetWidth / 2
-      ) {
-        crash.play()
-        crash.volume = 0.01
-        if (Number(asteroidRem[0]) > 8) {
-          asteroid.style.fontSize = asteroidRem[0] - 5 + 'rem'
-          container.removeChild(laser)
-        } else {
-          counter.textContent = Number(counter.innerHTML) + 1
-          container.removeChild(asteroid)
-          container.removeChild(laser)
-          window.clearInterval(asteroidFall.timer)
-          createAsteroid()
-        }
-      }
-    }
-
-    laser.style.top = positionLaserY - 4 + 'px'
-    if (positionLaserY < -10) {
-      container.removeChild(laser)
-    }
-  }, 0.1)
-}
-
-let createAsteroid = () => {
-  let asteroidShape = Math.floor(Math.random() * 6) + 1
-  let shape
-  switch (asteroidShape) {
-    case 1:
-      shape = '🌑'
-      break
-    case 2:
-      shape = '😈'
-      break
-    case 3:
-      shape = '🦂'
-      break
-    case 4:
-      shape = '👹'
-      break
-    case 5:
-      shape = '🤡'
-      break
-    case 6:
-      shape = '🧞‍♂️'
-      break
-    default:
-      break
-  }
-
-  let asteroid = document.createElement('div')
-  let asteroidSize = Math.floor(Math.random() * 20) + 3
-  asteroid.classList.add('asteroid')
-  asteroid.innerHTML = shape
-  asteroid.style.left =
-    Math.floor(Math.random() * window.innerWidth - 20) + 20 + 'px'
-  asteroid.style.fontSize = asteroidSize + 'rem'
-  container.insertAdjacentElement('beforeend', asteroid)
-  let randomNumber = Math.floor(Math.random() * 8) + 1
-  clearInterval(asteroidFall.timer)
-  asteroidFall(randomNumber, asteroid, laserFunction)
-}
-createAsteroid()
-let asteroid = document.querySelector('.asteroid')
-
+//Key
 document.addEventListener('keydown', event => {
   let positionShipX = ship.offsetLeft
-  let positionShipY = ship.offsetTop
-  if (event.keyCode === 37) {
+  if (event.key === 'ArrowLeft') {
     ship.style.left = positionShipX - 40 + 'px'
   }
-  if (event.keyCode === 39) {
+  if (event.key === 'ArrowRight') {
     ship.style.left = positionShipX + 40 + 'px'
   }
-  if (event.keyCode === 32) {
-    laserFunction(asteroidFall)
+  if (event.key === ' ') {
+    console.log('Space')
   }
 })
 
-document.addEventListener('click', event => {
-  laserFunction(asteroidFall)
-})
-
-gameover.addEventListener('click', () => {
-  for (let i = 1; i < 1000; i++) window.clearInterval(i)
-  lasers = document.querySelectorAll('.laser')
-  lasers.forEach(el => {
-    container.removeChild(el)
-  })
-  gameover.style.display = 'none'
-  counter.textContent = 0
-  death = 3
-  stars = ['⭐', '⭐', '⭐']
-  showStars()
-
-  createAsteroid()
-})
-
-window.addEventListener('mousemove', event => {
+//Mouse ship movement
+document.addEventListener('mousemove', event => {
   ship.style.left = event.clientX + 'px'
 })
 
+//Touch ship movement
 ship.addEventListener('touchmove', event => {
   ship.style.left = Math.floor(event.touches[0].clientX) + 'px'
 })
 
-mars.addEventListener('click', () => {
-  container.style.backgroundImage =
-    'url(https://alexejklass.github.io/dottaghedestroyer/img/mars.jpg)'
-})
-space.addEventListener('click', () => {
-  container.style.backgroundImage =
-    'url(https://alexejklass.github.io/dottaghedestroyer/img/galaxy.jpg)'
-})
+//Earth background
 earth.addEventListener('click', () => {
-  container.style.backgroundImage =
-    'url(https://alexejklass.github.io/dottaghedestroyer/img/earth.jpg)'
+  videoSource.setAttribute('src', 'video/earth.mp4')
+  videoContainer.load()
+})
+//Mars background
+mars.addEventListener('click', () => {
+  videoSource.setAttribute('src', 'dfg')
+  videoContainer.load()
+})
+//Space background
+space.addEventListener('click', () => {
+  videoSource.setAttribute('src', 'video/earth.mp4')
+  videoContainer.load()
 })
